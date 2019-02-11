a15067924704778b15e876613dc7ccd972c6aa0e
MoHaMed HaMdy <mohamedhamdy.o1997@gmail.com>
2019-02-12 00:07:00 +0200

    add reset password

--- app/Http/Controllers/AuthApi/AuthController.php
+++ app/Http/Controllers/AuthApi/AuthController.php
@@ -51,68 +51,6 @@ protected $user;
         ]);
     }
 
-    public function register(Request $request)
-    {
-        $this->validate($request ,[
-            'firstName' => 'required |string | max:50 | min:5',
-            'lastName' => 'required |string | max:50 | min:5',
-            'faculty' => 'required |string',
-            'university' => 'required |string',
-//            'DOB' => 'date_format:Y-M-D|before:today',
-            'email' => 'required |string|email|max:255| unique:users',
-            'password'=>'required|confirmed|string|min:6',
-            'password_confirmation'=>'sometimes|required_with:password',
-        ]);
-         //if position EX-com
-        if ($request->input('position')=='EX-com') {$this->validate($request, ['EX-comOptions' => 'required']);}
-
-        //if position High board and the committee was chosen RAS, PES, WIE:
-        if ($request->input('position')=='highBoard'&& ($request->input('committee')=='RAS' || 'PES' || 'WIE'))
-        {$this->validate($request, ['highBoardOptions' => 'required']);}
-
-        $user= new User();
-        $user->firstName= $request->input('firstName');
-        $user->lastName= $request->input('lastName');
-        $user->faculty= $request->input('faculty');
-        $user->university= $request->input('university');
-        $user->DOB= $request->input('DOB');
-        $user->position= $request->input('position');
-        $user->email=$request->input('email');
-        $user->password=app('hash')->make($request->input('password'));
-
-        if ($request->input('position')=='EX_com'){
-            $ex = new Ex_com_options();
-            $ex->ex_options = $request->input('ex_options');
-            if ($ex->ex_options!=null){
-                $user->save();
-                $ex->ex_id = $user->id;
-                $ex->save();
-            }else{return response()->json('error');}
-        }
-
-        if ($request->input('position')=='highBoard' || 'volunteer'){
-            $user->committee = $request->input('committee');
-        }
-
-        if ($request->input('position')=='highBoard' && ($request->input('committee')==('RAS'||'PES' || 'WIE') )){
-            $hb = new HighBoardOptions();
-            $hb->HB_options = $request->input('highBoardOptions');
-            if ($hb->HB_options != null){
-                $user->save();
-                $hb->HB_id = $user->id;
-                $hb->save();
-            }else{return response()->json('error');}
-        }
-
-        if ($request->input('position')!='EX_com' && ($request->input('position')=='highBoard' && ($request->input('committee')==('RAS'||'PES' || 'WIE') )))
-        {
-
-            $user->save();
-        }
-
-           return response()->json(['status' =>'success','user'=>$user]);
-        }
-
 //        Logout
     public function logout(Request $request)
     {
--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,18 @@
 
 use Illuminate\Http\Request;
 
-        //login & register
-Route::POST('login', 'ِِِAuthApi/AuthController@login');
+//login & register
+Route::POST('login', 'AuthApi\AuthController@login')->name('login');
 
-Route::POST('logout', 'ِِِAuthApi/AuthController@logout');
+Route::POST('logout', 'AuthApi\AuthController@logout');
 
 Route::Post('register', 'AuthApi\RegisterController@register');
 
 Route::get('register/verify/{confirmationCode}' ,  'AuthApi\ConfirmController@confirm');
+
+Route::post('/password/reset', 'AuthApi\ResetPasswordController@recover')->name('password.reset');
+
+Route::post('/password/reset/{reset_code}', 'AuthApi\ResetPasswordController@Reset')->name('password.reset');
 
 
 
